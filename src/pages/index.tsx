7d91aa245b522a3add705b9637f9c76ffad310a5
jarilton <jariltonpereira02@hotmail.com>
2021-11-04 18:03:14 -0300

    ajustes do firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../backend/db/ColecaoCliente";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 
 export default function Home() {
 
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
 
-  const clientes = [
-    new Cliente('Jamal', 23, '1'),
-    new Cliente('Thalia', 24, '2'),
-    new Cliente('Natalia', 28, '3'),
-    new Cliente('Marcos', 16, '4'),
-  ]
+  useEffect(obterTodos, [])
+
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
 
   function clienteSelecionado(cliente: Cliente) {
     setCliente(cliente)
     setVisivel('form')
   }
-  function clienteExcluido(cliente: Cliente) {
-    console.log(`Excluir ${cliente.nome}`)
+  async function clienteExcluido(cliente: Cliente) {
+    await repo.excluir(cliente)
+    obterTodos()
   }
 
   function novoCliente(cliente: Cliente) {
     setCliente(Cliente.vazio())
     setVisivel('form')
   }
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
-    setVisivel('tabela')
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente)
+    obterTodos()
   }
 
   return (
@@ -48,7 +56,7 @@ export default function Home() {
             <div className="flex justify-end">
               <Botao classname="mb-4" onClick={novoCliente}>
                 Novo Cliente
-               </Botao>
+              </Botao>
             </div>
             <Tabela clientes={clientes}
               clienteSelecionado={clienteSelecionado}
